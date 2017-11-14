c42562ca4184aab1f59531faf03d81e6a5078653
Julius Washington <jrdubs11@yahoo.com>
2017-11-14 18:32:23 -0500

    Create Odd Even.py

---
+++ Odd Even.py
@@ -0,0 +1,16 @@
+print("Let's multiple of 4, even, or odd?\n")
+ans = int(input("Gimme a number:\n"))
+if(ans % 4 == 0):
+    print("This is a multiple of 4!\n")
+elif (ans % 2 == 0):
+    print("This number is even!\n")
+else:
+    print("This number is odd!\n")
+
+print("Can you get a remainder of 0?")
+num = int(input("Gimme another number:\n"))
+check = int(input("One more number:\n"))
+if(num % check == 0 ):
+    print("Good Job!")
+else:
+    print("Try again!")
